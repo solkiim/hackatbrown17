80030c174b31507b023571d126eb13c622e43ff9
Carlos Rotger <carlos_rotger@brown.edu>
2017-02-04 19:26:26 -0500

    added d3 code

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
-
+    var tags = ["maga", "tbt", "mcm"];
+    var spheres = d3.select("a-scene").selectAll(".hashtag-sphere").data(tags);
+    spheres.enter()
+        .append("a-sphere")
+        .classed("hashtag-sphere", true)
+        .attr("position", function(d, i) {
+            return "0 " + "1 " + i;
+        })
+        .attr('radius', 1.25)
+        .attr('color', '#0000FF');
 });
